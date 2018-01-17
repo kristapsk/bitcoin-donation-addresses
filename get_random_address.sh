560e6245630721238967d28e90c272386bd6ffaa
Kristaps Kaupe <kristaps@blogiem.lv>
2018-01-17 04:46:25 +0200

    Allow to specify desired address format (P2PKH or P2SH)

--- get_random_address.sh
+++ get_random_address.sh
@@ -1,2 +1,10 @@
 #! /bin/bash
-grep -e '[13][a-km-zA-HJ-NP-Z1-9]\{25,34\}' README.md | cut -d '|' -f 3 | shuf -n 1
+format="`echo "$1" | tr '[:upper:]' '[:lower:]'`"
+if [ "$format" == "p2pkh" ]; then
+    format="1[a-km-zA-HJ-NP-Z1-9]\{25,34\}"
+elif [ "$format" == "p2sh" ]; then
+    format="3[a-km-zA-HJ-NP-Z1-9]\{25,34\}"
+else
+    format="[13][a-km-zA-HJ-NP-Z1-9]\{25,34\}"
+fi
+grep -e "$format" README.md | cut -d '|' -f 3 | shuf -n 1
