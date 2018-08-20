aa66e17ed66e1da35eac75cc77418750572afee3
Kristaps Kaupe <kristaps.k@intelligentsystems.lv>
2018-08-20 20:25:43 +0300

    Add ZeroLink / Wallet Wassabi (nopara73) + bech32 support

--- get_random_address.sh
+++ get_random_address.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 format="`echo "$1" | tr '[:upper:]' '[:lower:]'`"
 if [ "$format" == "p2pkh" ]; then
-    format="1[a-km-zA-HJ-NP-Z1-9]\{25,34\}"
+    format="[| ]1[a-km-zA-HJ-NP-Z1-9]\{25,39\}"
 elif [ "$format" == "p2sh" ]; then
-    format="3[a-km-zA-HJ-NP-Z1-9]\{25,34\}"
+    format="[| ][a-km-zA-HJ-NP-Z1-9]\{25,39\}"
+elif [ "$format" == "legacy" ]; then
+	format="[| ][13][a-km-zA-HJ-NP-Z1-9]\{25,39\}"
+elif [ "$format" == "bech32" ]; then
+	format="bc1[a-z0-9]\{8,87\}\|BC1[A-Z0-9]\{8,87\}"
 else
-    format="[13][a-km-zA-HJ-NP-Z1-9]\{25,34\}"
+    format="\([13][a-km-zA-HJ-NP-Z1-9]\{25,39\}\|bc1[a-z0-9]\{8,87\}\|BC1[A-Z0-9]\{8,87\}\)"
 fi
 grep -e "$format" README.md | cut -d '|' -f 3 | shuf -n 1
