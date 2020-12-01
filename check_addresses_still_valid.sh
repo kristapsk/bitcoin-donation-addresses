05ef63e3c400c8ddb4c2d14b0cf4310b390e328d
Kristaps Kaupe <kristaps@blogiem.lv>
2020-12-01 20:19:19 +0200

    Switch from tempfile to mktemp

--- check_addresses_still_valid.sh
+++ check_addresses_still_valid.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 LANG=POSIX grep -e '\([13][a-km-zA-HJ-NP-Z1-9]\{25,39\}\|bc1[a-z0-9]\{8,87\}\|BC1[A-Z0-9]\{8,87\}\)' README.md | cut -d '|' -f 3,4 | tr '|' ' ' | while read address url; do
-    datafile="`tempfile`"
+    datafile="$(mktemp)"
     wget -q -O $datafile $url
     if ! grep -qs $address $datafile; then
         echo "$address not found in $url"
